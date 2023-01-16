d12d8891b99dfcf8842de89ebc5f0bf7dcd033f8
Igor Dudetskiy <116377166+IgorDudetskiy@users.noreply.github.com>
2023-01-16 17:57:02 +0200

    17:56 first attempt

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+class A {
+    public static void show() {
+        System.out.println("a show(");
+    }
+}
+
+class B extends A {
+    public static void show() {
+        System.out.println("b sho");
+    }
+}
